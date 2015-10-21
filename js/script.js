1d1d93223863fef3f1b5d6a0396cccc8b43065a6
GabiOC <oconnor.gabriele@gmail.com>
2015-10-21 17:46:02 -0400

    Add all the things

--- js/script.js
+++ js/script.js
@@ -1,15 +1,8 @@
-//Write a javascript snippet that can be run in the console of a browser that does the following:
-//Extract the number of items in the cart, the cart total, and item images from the page.
-//Store them in javascript variables.
-
 $(function(){
-  // only get cart data and trigger overlay if user is on homepage & cart isn't empty
-  if(window.location.href == "http://marmot.com/" && getNumItems() > 0){
-    var numItems = getNumItems();
-    var cartTotal = getCartTotal();
-    var itemImages = getItemImages();
-    triggerOverlay(numItems, cartTotal, itemImages);
-  }
+  var numItems = getNumItems();
+  var cartTotal = getCartTotal();
+  var itemImages = getItemImages();
+  triggerOverlay(numItems, cartTotal, itemImages);
 });
 
 function getNumItems(){
@@ -17,13 +10,7 @@ function getNumItems(){
 }
 
 function getCartTotal(){
-  var $items = $("#cartItems").children(".item");
-  // parse html for items' prices, inclusive of decimal
-  var prices = $items.find("li:contains('Price')").map(function(){
-    return parseFloat($(this).html().match(/[0-9]+(\.[0-9][0-9]?)?/));
-  })
-  // sum total prices
-  return prices.reduce(function(a, b) { return a + b; })
+  return $("#hdrCartSubtotal").children("#subTotal").html();
 }
 
 function getItemImages(){
@@ -34,32 +21,82 @@ function getItemImages(){
   return images;
 }
 
-//Create a trigger that activates when the user scrolls into the bottom 10% of the page.
-function triggerOverlay(){
+function triggerOverlay(numItems, cartTotal, itemImages){
+  var lastScroll = 0 // compare current scroll vs. last scroll so overlay isn't re-triggered if user scrolls up
   $(window).scroll(function () {
-     if ($(window).scrollTop() + $(window).height() > $(document).height() * 0.9 ) {
-        openOverlay();
-     }
+    var currentScroll = $(window).scrollTop();
+      if ((currentScroll > $(document).height() * 0.9) && (currentScroll > lastScroll)){
+        $("#cart-modal").modal("show");
+        // remove previous cart data if modal previously generated
+        if ($(".cart-content").children().length > 0){
+          removeOldData();
+        }
+        // append cart data to modal
+        appendImages(itemImages);
+        appendNumItems(numItems);
+        appendTotal(cartTotal);
+
+        lastScroll = currentScroll;
+      }
   });
 }
 
-//The trigger should show a centered overlay on top of the site that displays the information
-// gathered above and two buttons. One button closes the overlay and the other takes the user to
-// the cart page.
-function openOverlay(){
-  $(function() {
-    $("#dialog-confirm").dialog({
-      resizable: false,
-      height:140,
-      modal: true,
-      buttons: {
-        "Delete all items": function() {
-          $( this ).dialog( "close" );
-        },
-        Cancel: function() {
-          $( this ).dialog( "close" );
-        }
-      }
-    });
-  });
+function removeOldData(){
+  $(".cart-content").empty();
 }
+
+function appendImages(images){
+  $(".cart-content").append("<div id='item-images'></div>");
+  images.each(function(i){
+    $("#item-images").append("<img src=" + images[i] + ">");
+  })
+}
+
+function appendNumItems(num){
+  $(".cart-content").append("<div id='item-num'></div>");
+  $("#item-num").append("<p>Current Items in Cart:" + num + "</p>");
+}
+
+function appendTotal(total){
+  $(".cart-content").append("<div id='item-total'></div>");
+  $("#item-total").append("<p>Cart Total:" + total + "</p>");
+}
+
+
+// // Code for live Marmot site
+// $(function(){
+//   // only get cart data and trigger overlay if user is on homepage & cart isn't empty
+//   if(window.location.href == "http://marmot.com/" && getNumItems() > 0){
+//     var numItems = getNumItems();
+//     var cartTotal = getCartTotal();
+//     var itemImages = getItemImages();
+//     triggerOverlay(numItems, cartTotal, itemImages);
+//   }
+// });
+//
+// function getNumItems(){
+//   return $("#cartItems").children(".item").length;
+// }
+//
+// function getCartTotal(){
+//   return $("#hdrCartSubtotal").children("#subTotal").html();
+// }
+//
+// function getItemImages(){
+//   var $items = $("#cartItems").children(".item");
+//   var images = $items.find(".itemImg").map(function(){
+//     return $(this).children("img").attr("src");
+//   })
+//   return images;
+// }
+//
+// function triggerOverlay(){
+//   var lastScroll = 0 // compare current scroll vs. last scroll so modal isn't triggered if user scrolls up
+//   $(window).scroll(function () {
+//     var currentScroll = $(window).scrollTop();
+//      if (currentScroll + $(window).height() > $(document).height() * 0.9 && (currentScroll > lastScroll)) {
+//        $('#cart-modal').modal('show');
+//        lastScroll = currentScroll;
+//      }
+//   });
+// }
